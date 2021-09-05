f67e60debe79c3b3e734ceca499bf0b6367fba85
Tatsuya Hotta <hotterdragon@gmail.com>
2021-09-05 14:00:22 +0900

    テストコードリファクタリング

--- Tests/BulkonyTests/ExporterTests.swift
+++ Tests/BulkonyTests/ExporterTests.swift
@@ -11,9 +11,7 @@ final class ExporterTests: XCTestCase {
     func testsCsvExporter() throws {
         let exporter = CsvExporter("/tmp/test.csv", ArrayRowGenerator(headers: getHeaders(), rows: getRows()))
         try exporter.export()
-        let fileHandle = FileHandle(forReadingAtPath: "/tmp/test.csv")
-        let data = fileHandle?.readDataToEndOfFile()
-        try fileHandle?.close()
+        let data = try getContents("/tmp/test.csv")
         XCTAssertNotEqual(nil, data)
         let result = "\u{FEFF}id,name,birthday\r\n1,\"\"\"Tatsuya Hotta\",1974/03/15\r\n2,\"Riho, Yoshioka\",1993/01/15\r\n3,\"Kana\r\nKurashina\",1987/12/23\r\n4,Kanna Hashimoto,1999/02/03"
         XCTAssertEqual(result, String(data: data!, encoding: .utf8)!)
@@ -23,9 +21,7 @@ final class ExporterTests: XCTestCase {
     func testsTsvExporter() throws {
         let exporter = TsvExporter("/tmp/test.tsv", ArrayRowGenerator(headers: getHeaders(), rows: getRows()))
         try exporter.export()
-        let fileHandle = FileHandle(forReadingAtPath: "/tmp/test.tsv")
-        let data = fileHandle?.readDataToEndOfFile()
-        try fileHandle?.close()
+        let data = try getContents("/tmp/test.tsv")
         XCTAssertNotEqual(nil, data)
         let result = "id\tname\tbirthday\n1\t\"\"\"Tatsuya Hotta\"\t1974/03/15\n2\tRiho, Yoshioka\t1993/01/15\n3\t\"Kana\r\nKurashina\"\t1987/12/23\n4\tKanna Hashimoto\t1999/02/03"
         XCTAssertEqual(result, String(data: data!, encoding: .utf8)!)
@@ -35,9 +31,7 @@ final class ExporterTests: XCTestCase {
     func testsXmlExporter() throws {
         let exporter = XmlExporter("/tmp/test.xml", ArrayRowGenerator(headers: getHeaders(), rows: getRows()))
         try exporter.export()
-        let fileHandle = FileHandle(forReadingAtPath: "/tmp/test.xml")
-        let data = fileHandle?.readDataToEndOfFile()
-        try fileHandle?.close()
+        let data = try getContents("/tmp/test.xml")
         XCTAssertNotEqual(nil, data)
         let result = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><root><row id=\"1\" name=\"&quot;Tatsuya Hotta\" birthday=\"1974/03/15\"></row><row id=\"2\" name=\"Riho, Yoshioka\" birthday=\"1993/01/15\"></row><row id=\"3\" name=\"Kana\r\nKurashina\" birthday=\"1987/12/23\"></row><row id=\"4\" name=\"Kanna Hashimoto\" birthday=\"1999/02/03\"></row></root>"
         XCTAssertEqual(result, String(data: data!, encoding: .utf8)!)
@@ -47,9 +41,7 @@ final class ExporterTests: XCTestCase {
     func testsJsonExporter() throws {
         let exporter = JsonExporter("/tmp/test.json", ArrayRowGenerator(headers: getHeaders(), rows: getRows()))
         try exporter.export()
-        let fileHandle = FileHandle(forReadingAtPath: "/tmp/test.json")
-        let data = fileHandle?.readDataToEndOfFile()
-        try fileHandle?.close()
+        let data = try getContents("/tmp/test.json")
         let json = try JSONSerialization.jsonObject(with: data!)
         let rows = json as! [[String: Any]]
         XCTAssertEqual(4, rows.count)
@@ -80,6 +72,17 @@ final class ExporterTests: XCTestCase {
             [4, "Kanna Hashimoto", formatter.string(from: formatter.date(from: "1999/02/03")!)]
         ]
         return AnySequence(data)
+    }
+
+    private func getContents(_ path: String) throws -> Data? {
+        let fileHandle = FileHandle(forReadingAtPath: path)
+        defer {
+            do {
+                try fileHandle?.close()
+            } catch {
+            }
+        }
+        return fileHandle?.readDataToEndOfFile()
     }
 }
 
