bcc540b3da62fd4764f3c0fdc69841b3d25edd84
Tatsuya Hotta <hotterdragon@gmail.com>
2021-09-10 20:28:38 +0900

    Validate関連クラスの追加

--- Sources/Bulkony/Import/RowHandler.swift
+++ Sources/Bulkony/Import/RowHandler.swift
@@ -6,7 +6,24 @@ import Foundation
 
 public protocol RowHandler {
 
+    typealias Errors = [Error]
+    typealias ErrorsCollection = [Errors]
+
     func handle(row: [String: String], lineNumber: UInt32, context: inout Context)
     func handle(row: [String], lineNumber: UInt32, context: inout Context)
+    func onError(row: [String: String], lineNumber: UInt32, rowErrors: Errors, context: inout Context)
+    func onError(row: [String], lineNumber: UInt32, rowErrors: Errors, context: inout Context)
+    
+}
+
+extension RowHandler {
+
+    func validate(row: [String: String], lineNumber: UInt32, context: inout Context) -> [Error] {
+        []
+    }
+
+    func validate(row: [String], lineNumber: UInt32, context: inout Context) -> [Error] {
+        []
+    }
 
 }
