40fc4dad8da13872fd849625178f2172403eaf2e
Tatsuya Hotta <hotterdragon@gmail.com>
2021-09-13 21:57:45 +0900

    Importerテストコードを追加

---
+++ Tests/BulkonyTests/ImporterTests.swift
@@ -0,0 +1,18 @@
+//
+// Created by 堀田竜也 on 2021/09/13.
+//
+
+@testable import Bulkony
+import Foundation
+import XCTest
+
+final class ImporterTests: XCTestCase {
+
+    func testsCsvImporter() {
+        let tempDir = NSTemporaryDirectory()
+        let filename = NSUUID().uuidString + ".csv"
+        _ = URL(fileURLWithPath: tempDir).appendingPathComponent(filename)
+
+    }
+
+}
